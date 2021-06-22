03355547c7b54195db46fc5dfa9b5f3a65d8ed30
Sh4ke96 <Sh4ke96@gmail.com>
2021-06-23 00:56:03 +0200

    add scroll up btn

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
-let countDownDate = new Date("Jul 31, 2021 15:37:25").getTime();
+const countDownDate = new Date("Jul 31, 2021 15:37:25").getTime();
 
-let countDownUpdate = setInterval(function () {
-  let dateNow = new Date().getTime();
-  let distance = countDownDate - dateNow;
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+const countDownUpdate = setInterval(function () {
+  const dateNow = new Date().getTime();
+  const distance = countDownDate - dateNow;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById("days").innerHTML = `${days} DAYS`;
   document.getElementById("hours").innerHTML = `${hours} HR`;
   document.getElementById("minutes").innerHTML = `${minutes} MIN`;
   document.getElementById("seconds").innerHTML = `${seconds} SEC`;
+});
+
+const scrollUpBtn = document.querySelector(".scroll__up");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+scrollUpBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
